7eabeac1af0c9483492e1c59f0f16eb4a4adff71
willhockey20 <willhockey122001@gmail.com>
2025-09-09 08:38:54 -0400

    working on getting swift lsp working

--- nvim/.config/nvim/lua/config/lsp/swift.lua
+++ nvim/.config/nvim/lua/config/lsp/swift.lua
@@ -1,7 +1,5 @@
 vim.lsp.config.swift = {
-        on_attach = on_attach, -- Re-use our on_attach function
-				capabilities = capabilities, -- Re-use our capabilities
-				cmd = { "xcrun", "sourcekit-lsp" },
-				filetypes = { "swift", "objectivec", "objectivecpp" },
+	cmd = { "xcrun", "sourcekit-lsp" },
+	filetypes = { "swift", "objectivec", "objectivecpp" },
 }
-vim.lsp.enable("sourcekit")
+vim.lsp.enable("sourcekit-lsp")
