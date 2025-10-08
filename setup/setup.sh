c7736d18386bd0b3ceca725f191fbf765540946b
William Ryan <willhockey122001@gmail.com>
2025-10-08 13:47:56 -0400

    Better organizied the order of operations
    Removed duplicate git clone and stow commands for plankton. Updated echo message for installing applications.

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,9 +11,16 @@ cd yay
 makepkg -si --noconfirm
 cd ~
 
+git clone https://github.com/wfryan/plankton.git
+cd ~/plankton
+
+echo "Stowing dotfiles"
+yay -S --noconfirm --needed stow
+stow hypr waybar starship zsh wofi nvim nwg-dock-hyprland ghostty
+cd ~
 echo "Beginning install of sysyem packages. ZSH, hyprland, etc. \n"
 yay -S --noconfirm --needed zsh zsh-autosuggestions zsh-syntax-highlighting
-yay -S --noconfirm --needed neovim tmux ripgrep fd fzf bat exa tree btop jq curl wget unzip zip stow less ffmpeg
+yay -S --noconfirm --needed neovim tmux ripgrep fd fzf bat exa tree btop jq curl wget unzip zip less ffmpeg
 yay -S --noconfirm --needed hyprland hyprlock hyprpaper hypridle
 yay -S --noconfirm --needed waybar grimblast wofi wl-clipboard ghostty starship nwg-dock-hyprland wlogout
 echo "Installing and flushing fonts\n"
@@ -21,12 +28,9 @@ yay -S --noconfirm --needed ttf-font-awesome ttf-jetbrains-mono-nerd ttf-firacod
 fc-cache -f -v
 echo "Installing networking and audio packages\n"
 yay -S --noconfirm --needed networkmanager networkmanager-dmenu pavucontrol sof-firmware alsa-firmware
-echo "Installing Dolphin, wallpaper engine, Firefox, Zen Browser, Steam, Spotify, Webcord/Discord\n"
-yay -S --noconfirm --needed linux-wallpaperengine dolphin firefox zen-browser steam spotify webcord discord
+echo "Installing Dolphin, wallpaper engine, Firefox, Zen Browser, Steam, Spotify, Discord\n"
+yay -S --noconfirm --needed linux-wallpaperengine dolphin firefox zen-browser steam spotify discord
 yay -S --noconfirm --needed go nerdfetch nitch zoxide
-git clone https://github.com/wfryan/plankton.git
-cd ~/plankton
-stow hypr waybar starship zsh wofi nvim nwg-dock-hyprland ghostty
 
 chsh -s $(which zsh)
 clear
